0f6829e1e23ed90754c6f93385e8c9c8b1001bd1
naufaladdhiya <naufal.addhiya@gmail.com>
2023-06-15 00:25:31 +0700

    feat: Create search note component

--- src/components/search-note.component.jsx
+++ src/components/search-note.component.jsx
@@ -4,10 +4,11 @@ const SearchNote = ({ onChangeHandler, keyword }) => {
       <h2 className="text-center text-2xl text-blue-400">Daftar Catatan</h2>
       <input
         type="text"
-        placeholder="Find your note..."
+        placeholder="Temukan catatan..."
         className="input input-bordered input-sm input-primary w-full max-w-7xl mt-3"
         onChange={onChangeHandler}
         value={keyword}
+        title="Cari Catatan"
       />
     </div>
   );
