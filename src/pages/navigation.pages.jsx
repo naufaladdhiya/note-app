958178fc3d6c55d209e554d9edac4b94eb0a697a
naufaladdhiya <naufal.addhiya@gmail.com>
2023-06-27 00:03:47 +0700

    feat: enable currentuser navbar

--- src/pages/navigation.pages.jsx
+++ src/pages/navigation.pages.jsx
@@ -1,22 +1,42 @@
-import React, { Fragment } from 'react';
-
-import { Link, Outlet } from 'react-router-dom';
-import { CgNotes } from 'react-icons/cg';
+/* eslint-disable arrow-body-style */
+import React, { Fragment, useContext } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { CgNotes } from "react-icons/cg";
+import { LocaleContext } from "../context/localization.context";
+import { UserContext } from "../context/user.context";
 
 function Navigation() {
+  const { locale, toggleLocale } = useContext(LocaleContext);
+  const { currentUser } = useContext(UserContext);
   return (
     <>
-      <div className="navbar bg-base-100 flex justify-between items-center px-7 shadow-2xl rounded-2xl py-4">
-        <div className="text-4xl text-warning">
-          <Link to="./" className="flex items-center gap-1">
-            <CgNotes />
-            <p>Notes</p>
-          </Link>
+      <div className="flex items-center justify-between py-4 shadow-2xl navbar bg-base-100 px-7 rounded-2xl">
+        <div className="navbar-start">
+          <div className="text-4xl text-warning">
+            <Link to="./" className="flex items-center gap-1">
+              <CgNotes />
+              <p>{locale === "id" ? "Catatan" : "Notes"}</p>
+            </Link>
+          </div>
         </div>
-        <div className="">
-          <Link to="./archieve">
-            <button className="btn btn-primary btn-md text-md" type="button">Arsip</button>
-          </Link>
+        {currentUser && (
+          <div className="gap-3 navbar-center">
+            <Link to="./">
+              <button className="btn btn-primary btn-md text-md" type="button">
+                {locale === "id" ? "Beranda" : "Home"}
+              </button>
+            </Link>
+            <Link to="./archieve">
+              <button className="btn btn-primary btn-md text-md" type="button">
+                {locale === "id" ? "Arsip" : "Archive"}
+              </button>
+            </Link>
+          </div>
+        )}
+        <div className="navbar-end">
+          <button type="button" onClick={toggleLocale}>
+            Toggle locale
+          </button>
         </div>
       </div>
       <Outlet />
